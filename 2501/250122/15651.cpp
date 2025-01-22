ca7099b8c3fe9b1a5862491c55490f4f2607d0ea
Kunhee, Lee <kitsune03@outlook.com>
2025-01-22 11:34:45 +0900

    dd

---
+++ 2501/250122/15651.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m; // 1~n에서 길이 m만큼
+vector<int> comb;
+
+// 중복 체크 없음
+void backtrack(int level, int start){
+    // level == m이면, 출력
+    if(level == m){
+        for(int i=0; i<m; i++){
+            cout << comb[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    else{
+        for(int i=1; i<=n; i++){
+            comb.push_back(i);
+            
+            backtrack(level+1, 1);
+            
+            comb.pop_back();
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    cin >> n >> m;
+    
+    backtrack(0, 1);
+}
